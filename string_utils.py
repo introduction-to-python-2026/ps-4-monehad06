cb249da3a127cfc75009c23eb3097264c470150f
monehad06 <mo.nehad06@gmail.com>
2025-11-18 21:05:49 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,6 +1,20 @@
 def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
+    start = 0
+    result = []
+    for end in range(1, len(formula)):
+        if formula[end].isupper():
+            result.append(formula[start:end])
+            start = end
+    result.append(formula[start:])
+    return result
+def split_at_digit(formula):
+    digit_location = 1
+    for char in formula[1:]:
+        if char.isdigit():
+            break
+        digit_location += 1
 
+    if digit_location == len(formula):
+        return formula, 1
 
-def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+    return formula[:digit_location], int(formula[digit_location:])
